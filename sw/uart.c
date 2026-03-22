02f27550a4464788e5d5ef251d42732b0178ec02
Nikita Aleksii <nikitaaleksij@gmail.com>
2026-03-22 11:03:14 -0400

    Implemented syscall stubs _write and _read so that C standard library functions can output and input through the UART

--- sw/uart.c
+++ sw/uart.c
@@ -1,19 +1,52 @@
-#define UART_TX   (*(volatile unsigned int *) 0x10000000)
-#define UART_STAT (*(volatile unsigned int *) 0x10000004)
-#define UART_RX   (*(volatile unsigned int *) 0x10000008)
+// Hardware driver for UART that directly talks to the hardware registers
 
-void uart_putc(char c){
-    while (UART_STAT & 0x1);
-    UART_TX = (unsigned int) c;
-}
+#include "uart.h"
 
-void uart_puts(const char *s){
-    while (*s){
-        uart_putc(*s++);
+#define UART_TX   (* (volatile uint32_t *) 0x10000000)
+#define UART_STAT (* (volatile uint32_t *) 0x10000004)
+#define UART_RX   (* (volatile uint32_t *) 0x10000008)
+
+// Status bits
+#define UART_TX_BUSY (1u << 0)
+#define UART_RX_EMPTY (1u << 1)
+#define UART_RX_FULL (1u << 2)
+#define UART_TX_EMPTY (1u << 3)
+#define UART_TX_FULL (1u << 4)
+
+int _write(int fildes, const void *buf, size_t nbyte) {
+    if (fildes != STDOUT_FILENO && fildes != STDERR_FILENO)
+        return -1;
+    
+    const uint8_t *p = buf;
+
+    for (size_t i = 0; i < nbyte; i++){
+        while(UART_STAT & UART_TX_FULL);
+        UART_TX = p[i];
     }
+
+    return (int) nbyte;
 }
 
-char uart_getc(void){
-    while (!(UART_STAT >> 1 & 0x1));
-    return (char) UART_RX;
+int _read(int fildes, void *buf, size_t nbyte){
+    if (fildes != STDIN_FILENO)
+        return -1;
+    
+    if (nbyte == 0)
+        return 0;
+
+    uint8_t *p = buf;
+    size_t i = 0;
+
+    while (i < nbyte) {
+        if (UART_STAT & UART_RX_EMPTY) {
+            if (i == 0)
+                continue;
+            
+            // Return partial 
+            break;
+        }
+        p[i++] = (uint8_t) UART_RX;
+    }
+
+    return (int) i;
 }
--- sw/uart.h
+++ sw/uart.h
@@ -1,8 +1,14 @@
 #ifndef UART_H
 #define UART_H
 
-void uart_putc(char c);
-void uart_puts(const char *s);
-char uart_getc(void);
+#include <stdint.h>
+#include <sys/types.h>
+
+#define STDIN_FILENO 0
+#define STDOUT_FILENO 1
+#define STDERR_FILENO 2
+
+extern int _write(int fildes, const void *buf, size_t nbyte);
+extern int _read(int fildes, void *buf, size_t nbyte);
 
 #endif
